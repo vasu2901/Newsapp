bdc608b954f0659cc246bb54dfbb3c5953dc2cd4
Vighnesh Vasu Vats <vasu8479sharma@gmail.com>
2023-04-14 13:07:40 +0530

    Final changes

--- client/src/components/NewsItem.js
+++ client/src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
           <img src={!imageurl ? "https://images.moneycontrol.com/static-mcnews/2022/07/LIVE-NEWS-BLOG-1-770x433.jpg" : imageurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title.slice(0,title.indexOf("-"))}<span className="position-absolute top-0 start-99 translate-middle badge rounded-pill bg-danger">
-              {source}</span>
+              {source.slice(0,source.indexOf("-"))}</span>
             </h5>
             <p className="card-text">{description}</p>
             <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
